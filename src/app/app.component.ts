b21aede2833315ab9082da0610da92b21ca13f4c
Denis <Kluev1179@gmail.com>
2024-08-23 08:29:04 +0300

    TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,30 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'TS';
+  howMuchILoveYou(nbPetals: number): string {
+    if (nbPetals > 6) {
+      nbPetals = nbPetals % 6 !== 0 ? nbPetals % 6 : 6;
+    }
+
+    console.log(nbPetals);
+
+    switch (nbPetals) {
+      case 1:
+        return 'I love you';
+      case 2:
+        return 'a little';
+      case 3:
+        return 'a lot';
+      case 4:
+        return 'passionately';
+      case 5:
+        return 'madly';
+      case 6:
+        return 'not at all';
+    }
+    return '';
+  }
 }
